5e4cf22476fb316079d8cf102eeae785ddfeffde
Tommy McG <thomasjmcgovern98@gmail.com>
2022-04-05 12:34:39 -0400

    HTML skeleton and excuse fetch

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+var excuseHistory = [];
+
+// Fetch a randomly generated excuse
+var getExcuse = function() {
+    fetch("https://excuser.herokuapp.com/v1/excuse").then(function(response) {
+        if(response.ok) {
+            response.json().then(function(data) {
+                console.log(data);
+                addToHistory(data[0].id, data[0].excuse);
+            })
+        }
+    })
+}
+
+// Store an excuse in our history array
+var addToHistory = function(id, excuse) {
+    excuseHistory.push({
+        id: id,
+        excuse: excuse
+    })
+    console.log(excuseHistory);
+}
+
+getExcuse();
